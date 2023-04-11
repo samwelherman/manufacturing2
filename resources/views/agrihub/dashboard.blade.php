aba874bf5765e3ac80c202cea7681ce5529f92da
samwelherman85 <samwelherman85@gmail.com>
2023-04-10 23:21:19 -0700

    adding cards to dashboard

--- resources/views/agrihub/dashboard.blade.php
+++ resources/views/agrihub/dashboard.blade.php
@@ -1,42 +1,238 @@
 @extends('layouts.master')
 
 @section('content')
-
     <section class="section">
+       
         <div class="row ">
-
-
-
-
-
-
           
-
-
-            
-
-           
-
-
+            <div class="col-lg-3 col-sm-6 ">
+                <div class="card  border-0 rounded-0">
+                    <div class="card-header p-3 pt-2">
+                        {{-- <div class="icon icon-lg icon-shape bg-secondary text-dark shadow-dark text-center border-radius-xl mt-n4 position-absolute">
+                    <i class="ph-money opacity-10 text-black"></i>
+                  </div> --}}
+                        <div class="text-end pt-1">
+                            <p class="text-sm mb-0 text-capitalize"> LPO </p>
+                            <h4 class="mb-0">5</h4>
+                        </div>
+                    </div>
+                    <hr class="dark horizontal my-0">
+                    <div class="card-footer p-3">
+                        <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+55% </span>than last
+                            Month</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 ">
+                <div class="card  border-0 rounded-0">
+                    <div class="card-header">
+                        {{-- <div class="icon icon-lg icon-shape bg-dark shadow-info text-center border-radius-xl mt-n4 position-absolute">
+                    <i class="ph-database  text-diana"></i>
+                  </div> --}}
+                        <div class="text-end pt-1">
+                            <p class="text-sm mb-0 text-capitalize"> INvoice</p>
+                            <h4 class="mb-0">43</h4>
+                        </div>
+                    </div>
+                    <hr class="dark horizontal my-0">
+                    <div class="card-footer p-3">
+                        <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+3% </span>than last
+                            month</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 ">
+                <div class="card  border-0 rounded-0">
+                    <div class="card-header">
+                        {{-- <div class="icon icon-lg icon-shape bg-success shadow-success text-center border-radius-xl mt-n4 position-absolute">
+                    <i class="ph-wallet text-light"></i>
+                  </div> --}}
+                        <div class="text-end pt-1">
+                            <p class="text-sm mb-0 text-capitalize "> Raw Material</p>
+                            <h4 class="mb-0">300 Kg</h4>
+                        </div>
+                    </div>
+                    <hr class="dark horizontal my-0">
+                    <div class="card-footer p-3">
+                        <p class="mb-0"><span class="text-danger text-sm font-weight-bolder">-2%</span> than last
+                            Month</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="card  border-0 rounded-0">
+                    <div class="card-header">
+                        {{-- <div class="icon icon-lg icon-shape bg-gradient-info shadow-info text-center border-radius-xl mt-n4 position-absolute">
+                    <i class="ph-envelope text-light"></i>
+                  </div> --}}
+                        <div class="text-end pt-1">
+                            <p class="text-sm mb-0 text-capitalize"> Requests</p>
+                            <h4 class="mb-0">TSH 43 M</h4>
+                        </div>
+                    </div>
+                    <hr class="dark horizontal my-0">
+                    <div class="card-footer p-3">
+                        <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+5% </span>than last
+                            Month</p>
+                    </div>
+                </div>
+            </div>
+    </div>
+    <div class="row ">
           
-
-
-
-
-
-
+        <div class="col-lg-3 col-sm-6 ">
+            <div class="card  border-0 rounded-0">
+                <div class="card-header p-3 pt-2">
+                    {{-- <div class="icon icon-lg icon-shape bg-secondary text-dark shadow-dark text-center border-radius-xl mt-n4 position-absolute">
+                <i class="ph-money opacity-10 text-black"></i>
+              </div> --}}
+                    <div class="text-end pt-1">
+                        <p class="text-sm mb-0 text-capitalize"> HDPE PIPE 160mm </p>
+                        <h4 class="mb-0">145 PC</h4>
+                    </div>
+                </div>
+                <hr class="dark horizontal my-0">
+                <div class="card-footer p-3">
+                    <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+55% </span>than last
+                        Month</p>
+                </div>
+            </div>
         </div>
+        <div class="col-lg-3 col-sm-6 ">
+            <div class="card  border-0 rounded-0">
+                <div class="card-header">
+                    {{-- <div class="icon icon-lg icon-shape bg-dark shadow-info text-center border-radius-xl mt-n4 position-absolute">
+                <i class="ph-database  text-diana"></i>
+              </div> --}}
+                    <div class="text-end pt-1">
+                        <p class="text-sm mb-0 text-capitalize"> 	HDPE PIPE 110mm</p>
+                        <h4 class="mb-0"> 89 PC</h4>
+                    </div>
+                </div>
+                <hr class="dark horizontal my-0">
+                <div class="card-footer p-3">
+                    <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+3% </span>than last
+                        month</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 ">
+            <div class="card  border-0 rounded-0">
+                <div class="card-header">
+                    {{-- <div class="icon icon-lg icon-shape bg-success shadow-success text-center border-radius-xl mt-n4 position-absolute">
+                <i class="ph-wallet text-light"></i>
+              </div> --}}
+                    <div class="text-end pt-1">
+                        <p class="text-sm mb-0 text-capitalize ">TIGER PIPE 20mm</p>
+                        <h4 class="mb-0">34 PC</h4>
+                    </div>
+                </div>
+                <hr class="dark horizontal my-0">
+                <div class="card-footer p-3">
+                    <p class="mb-0"><span class="text-danger text-sm font-weight-bolder">-2%</span> than last
+                        Month</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+            <div class="card  border-0 rounded-0">
+                <div class="card-header">
+                    {{-- <div class="icon icon-lg icon-shape bg-gradient-info shadow-info text-center border-radius-xl mt-n4 position-absolute">
+                <i class="ph-envelope text-light"></i>
+              </div> --}}
+                    <div class="text-end pt-1">
+                        <p class="text-sm mb-0 text-capitalize"> E-100</p>
+                        <h4 class="mb-0"> 43 KG</h4>
+                    </div>
+                </div>
+                <hr class="dark horizontal my-0">
+                <div class="card-footer p-3">
+                    <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+5% </span>than last
+                        Month</p>
+                </div>
+            </div>
+        </div>
+</div>
+    <div class="row ">
+          
+        <div class="col-lg-3 col-sm-6 ">
+            <div class="card  border-0 rounded-0">
+                <div class="card-header p-3 pt-2">
+                    {{-- <div class="icon icon-lg icon-shape bg-secondary text-dark shadow-dark text-center border-radius-xl mt-n4 position-absolute">
+                <i class="ph-money opacity-10 text-black"></i>
+              </div> --}}
+                    <div class="text-end pt-1">
+                        <p class="text-sm mb-0 text-capitalize"> RECYCLE </p>
+                        <h4 class="mb-0"> 127 KG</h4>
+                    </div>
+                </div>
+                <hr class="dark horizontal my-0">
+                <div class="card-footer p-3">
+                    <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+55% </span>than last
+                        Month</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 ">
+            <div class="card  border-0 rounded-0">
+                <div class="card-header">
+                    {{-- <div class="icon icon-lg icon-shape bg-dark shadow-info text-center border-radius-xl mt-n4 position-absolute">
+                <i class="ph-database  text-diana"></i>
+              </div> --}}
+                    <div class="text-end pt-1">
+                        <p class="text-sm mb-0 text-capitalize"> SCRAPS1</p>
+                        <h4 class="mb-0"> 89 KG</h4>
+                    </div>
+                </div>
+                <hr class="dark horizontal my-0">
+                <div class="card-footer p-3">
+                    <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+3% </span>than last
+                        month</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 ">
+            <div class="card  border-0 rounded-0">
+                <div class="card-header">
+                    {{-- <div class="icon icon-lg icon-shape bg-success shadow-success text-center border-radius-xl mt-n4 position-absolute">
+                <i class="ph-wallet text-light"></i>
+              </div> --}}
+                    <div class="text-end pt-1">
+                        <p class="text-sm mb-0 text-capitalize ">  Screps</p>
+                        <h4 class="mb-0">38 KG</h4>
+                    </div>
+                </div>
+                <hr class="dark horizontal my-0">
+                <div class="card-footer p-3">
+                    <p class="mb-0"><span class="text-danger text-sm font-weight-bolder">-2%</span> than last
+                        Month</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+            <div class="card  border-0 rounded-0">
+                <div class="card-header">
+                    {{-- <div class="icon icon-lg icon-shape bg-gradient-info shadow-info text-center border-radius-xl mt-n4 position-absolute">
+                <i class="ph-envelope text-light"></i>
+              </div> --}}
+                    <div class="text-end pt-1">
+                        <p class="text-sm mb-0 text-capitalize"> Requests</p>
+                        <h4 class="mb-0">TSH 43 M</h4>
+                    </div>
+                </div>
+                <hr class="dark horizontal my-0">
+                <div class="card-footer p-3">
+                    <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+5% </span>than last
+                        Month</p>
+                </div>
+            </div>
+        </div>
+</div>
 
+        
+    </section>
         <!-- /quick stats boxes -->
+    @endsection
 
-
-
-
- 
-@endsection
-
-@section('scripts')
- 
-   
-    
-@endsection
+    @section('scripts')
+    @endsection
